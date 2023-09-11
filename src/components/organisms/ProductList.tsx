fc0bf6377f10949d7eda634a6beb397ba90c5ecf
Andrzej Chuchra <andrzej.chuchra@gmail.com>
2023-09-11 20:01:05 +0200

    added datatestid

--- src/components/organisms/ProductList.tsx
+++ src/components/organisms/ProductList.tsx
@@ -3,7 +3,7 @@ import { ProductListItem } from "@/components/molecules/ProductListItem";
 
 export const ProductList = ({ products }: { products: ProductItem[] }) => {
 	return (
-		<ul className="flex flex-wrap justify-around">
+		<ul className="flex flex-wrap justify-around" data-testid="products-list">
 			{products.map((product) => (
 				<li key={product.id}>
 					<ProductListItem product={product}></ProductListItem>
